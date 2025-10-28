7f1dc8e9c40a0e98a0fa8ee53d1b32829eb81e1a
bean-123 <amy.platt@hotmail.com>
2025-10-28 18:33:02 +0200

    Add map in appjsx and styling for cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,26 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Person from "./components/PersonCard";
+import PersonCard from "./components/PersonCard";
+import data from "./data";
+import { useState } from "react";
 
 function App() {
+  const [employees, setEmployees] = useState(data);
+
   return (
     <>
       <Header headertext="hrApp" />
       <div className="container">
-        <Person
-          name="Amy"
-          title="CEO"
-          salary="4000"
-          phone="044 238 4535"
-          email="amy.platt@hotmail.com"
-          animal="Cat"
-        />
-        <Person
-          name="Eliott"
-          title="Boss"
-          salary="4000"
-          phone="+386 768 2389"
-          email="eliott@french.com"
-          animal="Cat and Dog"
-        />
-        <Person
-          name="Nathaniel"
-          title="Sales"
-          salary="5000"
-          phone="+687 888 4532"
-          email="loverboy@wales.com"
-          animal="Dogs"
-        />
+        {employees.map((employee) => (
+          <PersonCard
+            name={employee.name}
+            title={employee.title}
+            salary={employee.salary}
+            age={employee.age}
+            animal={employee.animal}
+          />
+        ))}
       </div>
       <Footer />
     </>
